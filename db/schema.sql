8358ff3f0fc4492276db92e65b5043013f5c50f2
JosephOkumu <jokumu25@gmail.com>
2025-01-11 05:12:39 +0300

    create post categories table

--- db/schema.sql
+++ db/schema.sql
@@ -35,4 +35,14 @@ CREATE TABLE IF NOT EXISTS categories (
     description TEXT                          
 );
 
+-- POST_CATEGORIES Table
+CREATE TABLE IF NOT EXISTS post_categories (
+    post_id INTEGER NOT NULL,                  
+    category_id INTEGER NOT NULL,              
+    PRIMARY KEY (post_id, category_id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+
 
