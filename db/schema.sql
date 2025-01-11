67f34cbc9442b283ffecf7e36a40ab98c643b579
JosephOkumu <jokumu25@gmail.com>
2025-01-11 05:09:24 +0300

    Create posts table

--- db/schema.sql
+++ db/schema.sql
@@ -7,4 +7,14 @@ CREATE TABLE IF NOT EXISTS users (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
 
+-- POSTS Table
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,     
+    user_id INTEGER NOT NULL,                 
+    title TEXT NOT NULL,                      
+    content TEXT NOT NULL,                    
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) 
+);
+
 
