c6db4c57f7c453b937960f2a58b7b26f73a04845
bnyatoro <nyatorobravian@gmail.com>
2025-01-17 12:24:38 +0300

    refactor: remove openBrowser function and clean up imports

--- main.go
+++ main.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os/exec"
-	"runtime"
+	
 
 	"forum/db"
-	"forum/internals/auth"
 	"forum/internals/post"
+	"forum/internals/auth"
 )
 
 func main() {
@@ -39,26 +38,8 @@ func main() {
 
 	// mux.Handle("/dashboard", auth.Middleware(protected))
 
-	// openBrowser("http://localhost:8080/")
 	fmt.Println("Server running http://localhost:8080/  and go to /login to login")
 	http.ListenAndServe(":8080", mux)
 }
 
-func openBrowser(url string) {
-	var err error
-	fmt.Println(runtime.GOOS)
-	switch runtime.GOOS {
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
 
-	if err != nil {
-		fmt.Printf("Failed to open browser: %v\n", err)
-	}
-}
