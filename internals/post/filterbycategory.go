e963eb8668e90abf78907e668351af28fb546d3f
ombima56 <ombimahillary6@gmail.com>
2025-01-30 22:28:55 +0300

    refactor: centralize SQL queries for FetchPostsByCategory function in queries.go for maintainability

--- internals/post/filterbycategory.go
+++ internals/post/filterbycategory.go
@@ -16,46 +16,8 @@ func FetchPostsByCategory(category string, userID int64) ([]Post, error) {
 	category = strings.TrimSpace(category)
 	category = strings.ToLower(category)
 
-	// SQL query to fetch posts for a specific category with additional fields, including the user's reaction.
-	query := `
-		SELECT 
-			p.id, 
-			p.title, 
-			p.content, 
-			u.username, 
-			p.created_at,
-			COALESCE(c.comment_count, 0) AS comment_count,
-			COALESCE(r.likes, 0) AS likes,
-			COALESCE(r.dislikes, 0) AS dislikes,
-			COALESCE(pr.reaction_type, '') AS user_reaction -- Fetch user's reaction or default to empty string
-		FROM posts p
-		JOIN users u ON p.user_id = u.id
-		JOIN post_categories pc ON p.id = pc.post_id
-		JOIN categories cat ON pc.category_id = cat.id
-		LEFT JOIN (
-			SELECT post_id, COUNT(*) AS comment_count 
-			FROM comments 
-			GROUP BY post_id
-		) c ON p.id = c.post_id
-		LEFT JOIN (
-			SELECT 
-				post_id, 
-				SUM(CASE WHEN reaction_type = 'LIKE' THEN 1 ELSE 0 END) AS likes,
-				SUM(CASE WHEN reaction_type = 'DISLIKE' THEN 1 ELSE 0 END) AS dislikes
-			FROM post_reactions
-			GROUP BY post_id
-		) r ON p.id = r.post_id
-		LEFT JOIN (
-			SELECT post_id, reaction_type
-			FROM post_reactions
-			WHERE user_id = ?
-		) pr ON p.id = pr.post_id
-		WHERE LOWER(cat.name) = ?
-		ORDER BY p.id DESC;
-	`
-
 	// Using db.DB to query the database
-	rows, err := db.DB.Query(query, userID, category)
+	rows, err := db.DB.Query(GetFilteredPostsByCategory, userID, category)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch posts by category: %w", err)
 	}
