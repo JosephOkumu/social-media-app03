bb1a0d7a529a25add575eee1a031e32db6283871
hiombima <ombimahillary52@gmail.com>
2025-02-03 12:52:20 +0300

    feat: add routes package for better maintainability

---
+++ internals/routes/routes.go
@@ -0,0 +1,43 @@
+package routes
+
+import (
+	"net/http"
+
+	"forum/internals/auth"
+	"forum/internals/comments"
+	"forum/internals/post"
+)
+
+func RegisteringRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", post.ServeHomePage)
+	mux.HandleFunc("/about", post.ServeAboutPage)
+
+	// Post Routes.
+	mux.HandleFunc("/posts", post.ServePosts)
+	mux.HandleFunc("/view-post", post.ViewPost)
+	mux.HandleFunc("/create-post-form", auth.Middleware(http.HandlerFunc(post.ServeCreatePostForm)))
+	mux.HandleFunc("/upload-image", auth.Middleware(http.HandlerFunc(post.UploadImage)))
+	mux.HandleFunc("/categories", post.ServeCategories)
+	mux.HandleFunc("/create-post", auth.Middleware(http.HandlerFunc(post.CreatePost)))
+	mux.HandleFunc("/post/react", auth.Middleware(http.HandlerFunc(post.ReactToPost)))
+
+	// Auth Routes.
+	mux.HandleFunc("/signup", auth.Signup)
+	mux.HandleFunc("/login", auth.Login)
+	mux.HandleFunc("/logout", auth.Logout)
+
+	// Filter Routes.
+	mux.HandleFunc("/category", post.ViewPostsByCategory)
+	mux.HandleFunc("/userfilter", auth.Middleware(http.HandlerFunc(post.FilterbyUser)))
+	mux.HandleFunc("/likesfilter", auth.Middleware(http.HandlerFunc(post.FilterbyLikes)))
+
+	// Comment Routes
+	mux.HandleFunc("/comments", comments.GetComments)
+	mux.HandleFunc("/comments/create", auth.Middleware(http.HandlerFunc(comments.CreateComment)))
+	mux.HandleFunc("/comments/react", auth.Middleware(http.HandlerFunc(comments.ReactToComment)))
+
+	// static
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	return mux
+}
