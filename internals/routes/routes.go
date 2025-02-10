34e1b884b601228b81223950bcd275eff5aff921
hilaokello <hilaryokello091@gmail.com>
2025-02-10 11:18:10 +0300

    feat: handler for serving static files

--- internals/routes/routes.go
+++ internals/routes/routes.go
@@ -2,9 +2,14 @@ package routes
 
 import (
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"forum/internals/auth"
 	"forum/internals/comments"
+	"forum/internals/fails"
 	"forum/internals/post"
 )
 
@@ -50,6 +55,52 @@ func RegisteringRoutes() *http.ServeMux {
 	mux.HandleFunc("/comments/react", auth.Middleware(http.HandlerFunc(comments.ReactToComment)))
 
 	// static
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux.HandleFunc("/static/", serveStatic)
 	return mux
+}
+
+// serveStatic serves static files
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		fails.ErrorPageHandler(w, r, http.StatusMethodNotAllowed)
+		return
+	}
+	// Remove the /static/ prefix from the URL path
+	filePath := path.Join("static", strings.TrimPrefix(r.URL.Path, "/static/"))
+
+	// Check if the file exists and is not a directory
+	info, err := os.Stat(filePath)
+	if err != nil || info.IsDir() {
+		fails.ErrorPageHandler(w, r, http.StatusForbidden)
+		return
+	}
+
+	// Check the file extension and set the appropriate Content-Type
+	ext := filepath.Ext(filePath)
+	switch ext {
+	case ".css":
+		w.Header().Set("Content-Type", "text/css")
+	case ".js":
+		w.Header().Set("Content-Type", "application/javascript")
+	case ".png":
+		w.Header().Set("Content-Type", "image/png")
+	case ".jpg", ".jpeg":
+		w.Header().Set("Content-Type", "image/jpeg")
+	case ".gif":
+		w.Header().Set("Content-Type", "image/gif")
+	case ".svg":
+		w.Header().Set("Content-Type", "image/svg+xml")
+	case ".webp":
+		w.Header().Set("Content-Type", "image/webp")
+	case ".bmp":
+		w.Header().Set("Content-Type", "image/bmp")
+	case ".ico":
+		w.Header().Set("Content-Type", "image/x-icon")
+	default:
+		fails.ErrorPageHandler(w, r, http.StatusForbidden)
+		return
+	}
+
+	// Serve the file
+	http.ServeFile(w, r, filePath)
 }
